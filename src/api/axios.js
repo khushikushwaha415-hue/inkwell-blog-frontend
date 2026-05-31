008ade49e31670e4d6c0bb9e07ce9d98726707bd
Khushi <khushikushwaha415@gmail.com>
2026-05-31 15:46:44 +0530

    fix: update production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: 'https://inkwell-blog-backend.onrender.com/api'
+});
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('inkwell_user'));
